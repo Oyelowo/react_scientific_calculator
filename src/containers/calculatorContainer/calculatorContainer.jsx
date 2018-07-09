e37f0407a05c23ca59be6fa09f36956d35998dc3
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-07-09 05:26:17 +0300

    handler name changed

--- src/containers/calculatorContainer/calculatorContainer.jsx
+++ src/containers/calculatorContainer/calculatorContainer.jsx
@@ -102,7 +102,7 @@ class CalculatorContainer extends Component {
         }
     }
 
-    changeToHypHandler = () => {
+    toggleHypHandler = () => {
         this.setState({
             trigsAreInverse: !this.state.trigsAreInverse
         })
@@ -135,7 +135,7 @@ class CalculatorContainer extends Component {
                     <div className='display'>{this.state.inputValue}</div>
                 </div>
                 <div className='extraKeys'>
-                    <button onClick={this.changeToHypHandler}>2nd</button>
+                    <button onClick={this.toggleToHypHandler}>2nd</button>
                     <button onClick={this.toggleDegreeRadanHandler}>{this.state.trigIsDegree
                             ? 'DEG'
                             : 'RAD'}</button>
