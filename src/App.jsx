743f25ce993df2e92d70f277437c7bee9718f324
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-07-11 11:49:07 +0300

    background image, h1 and footer added

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,18 @@ import CalculatorContainer from './containers/calculatorContainer/calculatorCont
 
 class App extends Component {
   render() {
-    return <CalculatorContainer />
+    return (
+      <div>
+        <h1>My ReactJS Scientific Calculator <i class="fas fa-calculator"></i></h1>
+        <CalculatorContainer/>
+        <footer>Coded by:
+          <a
+            href="https://www.linkedin.com/in/oyelowo-oyedayo/"
+            target="_blank"
+            rel="noopener noreferrer"> <i class="fab fa-linkedin"></i> Oyelowo Oyedayo</a>
+        </footer>
+      </div>
+    );
   }
 }
 
